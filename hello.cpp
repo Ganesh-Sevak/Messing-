1ec9f2f84b77f0b0d3242a27d3964d62677aea2f
Ganesh-Sevak <gsevak585@gmail.com>
2025-05-21 00:48:28 -0500

    Add Hello World example

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>   // std::cout, std::cin, std::cerr, std::endl
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    cout << "No more std:: prefix!\n";
+    vector<int> v = {1,2,3};
+    return 0;
+}
